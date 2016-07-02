59199b33d3d8b1251f0c0fcefbe39f2ed38f8015
Jannik Schürg <jannik.schuerg@posteo.de>
2016-07-02 15:47:02 +0200

    weather api auslagern

---
+++ src/cloudify/weather.clj
@@ -0,0 +1,20 @@
+(ns cloudify.weather
+    (:require [clojure.data.json :as json])
+    (:require [clj-http.client :as client]))
+    
+(def weatherURL "http://api.openweathermap.org/data/2.5/forecast?id=2946447&appid=a5982036f8e98737775ab26d08f4936d")
+
+(defn map-3h [main]
+    [(Math/round (- (get-in main [:main :temp]) 273.15))
+     (get-in main [:main :pressure])
+     (get-in main [:main :humidity])
+     (get-in main [:clouds :all])
+     (get-in main [:wind :speed])])
+
+(defn parse-data [body]
+    (filter #(not-any? nil? %1)
+        (map map-3h
+            (:list (json/read-str body :key-fn keyword)))))
+    
+(defn download-and-parse []
+    (parse-data ((client/get weatherURL) :body)))
